c4a3284f08019db1ba1a16045939d0e8f926c494
Syed Nizamudeen <nizamudeen.syed@globaltix.com>
2025-12-04 22:19:02 +0800

    first commit

---
+++ web/my-home-clock/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-10 bg-gray-100 min-h-screen">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Home Clock is working!
+      </h1>
+    </div>
+  )
+}
+
+export default App
