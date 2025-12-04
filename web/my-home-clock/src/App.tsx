554baa6848ed47d6e5e8201bb5903cf72b939bef
Syed Nizamudeen <nizamudeen.syed@globaltix.com>
2025-12-04 23:26:58 +0800

    Update App component header to include workflow test timestamp

--- web/my-home-clock/src/App.tsx
+++ web/my-home-clock/src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="p-10 bg-gray-100 min-h-screen">
       <h1 className="text-3xl font-bold text-blue-600">
-        Home Clock is working!
+        Home Clock is working! Test Workflow Thu Dec 4 11:26:31PM
       </h1>
     </div>
   )
