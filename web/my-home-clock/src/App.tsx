7f6dee4701d5d77be94d0d138f7a93c2d530e526
Syed Nizamudeen <nizamudeen.syed@globaltix.com>
2025-12-30 23:25:15 +0800

    feat: add Font Awesome icons for network status and enhance App layout

--- web/my-home-clock/src/App.tsx
+++ web/my-home-clock/src/App.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faServer, faSignal, faWifi } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   const [time, setTime] = useState(new Date());
@@ -30,39 +32,65 @@ function App() {
   const weekday = today.toLocaleString("default", { weekday: "long" });
 
   return (
-    <>
-      <div className="text-center p-4 border border-primary rounded-xl shadow-lg m-5 inline-block">
-        <span className="countdown text-7xl tracking-wider text-primary">
-          <span
-            style={{ "--value": hours, "--digits": 2 } as React.CSSProperties}
-            aria-live="polite"
-            aria-label={String(hours)}
-          >
-            {hours}
-          </span>
-          :
-          <span
-            style={{ "--value": minutes, "--digits": 2 } as React.CSSProperties}
-            aria-live="polite"
-            aria-label={String(minutes)}
-          >
-            {minutes}
-          </span>
-          :
-          <span
-            style={{ "--value": seconds, "--digits": 2 } as React.CSSProperties}
-            aria-live="polite"
-            aria-label={String(seconds)}
-          >
-            {seconds}
-          </span>
-        </span>
-        <span className="text-accent">{ampm}</span>
-        <div className="text-4xl text-secondary">
-          {weekday}, {date} {month}, {year}
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 min-w-[640px] min-h-screen">
+      <div className="border rounded-xl p-4 border-primary md:col-span-2 flex flex-col items-center justify-center">
+        <div className="my-5">
+          <div>
+            <span className="countdown text-8xl font-bold tracking-wider text-primary">
+              <span
+                style={
+                  { "--value": hours, "--digits": 2 } as React.CSSProperties
+                }
+                aria-live="polite"
+                aria-label={String(hours)}
+              >
+                {hours}
+              </span>
+              :
+              <span
+                style={
+                  { "--value": minutes, "--digits": 2 } as React.CSSProperties
+                }
+                aria-live="polite"
+                aria-label={String(minutes)}
+              >
+                {minutes}
+              </span>
+              :
+              <span
+                style={
+                  { "--value": seconds, "--digits": 2 } as React.CSSProperties
+                }
+                aria-live="polite"
+                aria-label={String(seconds)}
+              >
+                {seconds}
+              </span>
+            </span>
+            <span className="text-2xl text-accent">{ampm}</span>
+          </div>
+          <div className="text-3xl text-center text-secondary">
+            {weekday}, {date} {month}, {year}
+          </div>
+          <div className="flex justify-center mt-5">
+            <div className="badge badge-success badge-xl">
+              <FontAwesomeIcon icon={faWifi} className="text-neutral" /> Wifi 5G{" "}
+              <FontAwesomeIcon icon={faServer} className="text-neutral" /> 2
+              Devices{" "}
+              <FontAwesomeIcon icon={faSignal} className="text-neutral" /> 1
+              Gbps
+            </div>
+          </div>
         </div>
       </div>
-    </>
+      <div className="border rounded-xl p-4 border-primary flex flex-col items-center justify-center">
+        {/* 02 */}
+      </div>
+      <div className="border rounded-xl p-4 border-primary">{/* 03 */}</div>
+      <div className="border rounded-xl p-4 border-primary md:col-span-2">
+        {/* 04 */}
+      </div>
+    </div>
   );
 }
 
